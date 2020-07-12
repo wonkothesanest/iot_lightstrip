378388f6d37c8fe77af9832cb6e5a3cc5acdda63
wonkothesanest <dhagstrom@newengen.com>
2020-07-12 11:15:00 -0700

    nailed it

--- iot_eclipse_project/external/certificate_ota/README.md
+++ iot_eclipse_project/external/certificate_ota/README.md
@@ -8,7 +8,8 @@ To Generate the Certificate:
 openssl req -x509 -newkey rsa:2048 -keyout ca_key.pem -out ca_cert.pem -days 365
 
 To Start the https server to host the file:
-openssl s_server -WWW -key ca_key.pem -cert ca_cert.pem -port 8070
+#openssl s_server -WWW -key ca_key.pem -cert ca_cert.pem -port 8070
+use the server.py program in this directory to start the server, you must supply a pass phrase.  Working dir is set to /home/pi/iot-binaries/ where the binaries should be
 
 Test the connection with Curl (on localhost):
 curl --cacert /home/dustin/workspace/esp32/iot_lightstrip/iot_eclipse_project/external/certificate_ota/ca_cert.pem https://localhost:8070/ca_cert.pem
--- iot_eclipse_project/external/certificate_ota/server.py
+++ iot_eclipse_project/external/certificate_ota/server.py
@@ -2,19 +2,56 @@ import BaseHTTPServer, SimpleHTTPServer
 import ssl
 import argparse
 import os
+import time
+import threading
 
+keyfile = "/home/pi/iot_lightstrip/iot_eclipse_project/external/certificate_ota/ca_key.pem"
+certfile = "/home/pi/iot_lightstrip/iot_eclipse_project/external/certificate_ota/ca_cert.pem"
+binDir = "/home/pi/iot-binaries/"
+timeout = time.time() + 10*60
+
+
+class StoppableHTTPServer(BaseHTTPServer.HTTPServer):
+    def run(self):
+        try:
+            self.serve_forever()
+        except KeyboardInterrupt:
+            self.shutdown()
+            self.server_close()
+            pass
+        finally:
+            # Clean-up server (close socket, etc.)
+            self.server_close()
+
+
+###### MAIN CODE #######
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--port', dest='port', type= int,
     help= "Server Port", default= 8000)
 args = parser.parse_args()
 
-httpd = BaseHTTPServer.HTTPServer(('', args.port),
+os.chdir(binDir)
+
+httpd = StoppableHTTPServer(('', args.port),
         SimpleHTTPServer.SimpleHTTPRequestHandler)
 
 httpd.socket = ssl.wrap_socket (httpd.socket,
-        keyfile= "ca_key.pem",
-        certfile= "ca_cert.pem", server_side=True)
-print("Serving on 0.0.0.0 port ", args.port)
-httpd.serve_forever()
+        keyfile= keyfile,
+        certfile= certfile, server_side=True)
+thread = threading.Thread(None, httpd.serve_forever)
 
+print("Serving on raspberrypi.local port ", args.port)
+thread.start()
+
+while(time.time() < timeout):
+   try:
+      time.sleep(1)
+   except KeyboardInterrupt:
+      break
+
+httpd.shutdown()
+thread.join()
+
+
+print("Shutdown and Exiting")
 #https://github.com/espressif/esp-idf/issues/2625
